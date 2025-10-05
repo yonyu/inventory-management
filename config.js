d7d8d2b49e5d895c3e45b73c43994faff597ba75
Yong Yu <ypyu@hotmail.com>
2025-10-04 22:48:58 -0700

    Create the configuration file

---
+++ config.js
@@ -0,0 +1,8 @@
+const MONGODB_URI = "mongodb://192.168.1.89:27017/inventories";
+
+const API = "http://localhost:3000/api";
+
+module.exports = {
+    MONGODB_URI: MONGODB_URI,
+    API
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+import config from './config.js';
+
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    MONGODB_URI: config.MONGODB_URI,
+    API: config.API,
+  }
 };
 
 export default nextConfig;
