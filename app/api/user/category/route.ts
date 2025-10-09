9694796994f92e9e058399f4fe7ae06c60df137a
Yong Yu <ypyu@hotmail.com>
2025-10-09 13:17:02 -0700

    Add GET and POST api endpoint for category

---
+++ app/api/user/category/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from 'next/server';
+
+import { dbConnect } from '../../../../utils/dbConnect';
+
+import Category from '../../../../models/category';
+
+
+export async function GET() {
+    await dbConnect();
+
+    try {
+        const categories = await Category.find({}).sort({ createdAt: -1});
+        return NextResponse.json({ categories }, { status: 200 });
+
+    } catch(error: any) {
+        //console.log(error);
+        return NextResponse.json({ err: error.message }, { status: 500 });
+    }
+}
+
+export async function POST(req: Request) {
+    await dbConnect();
+
+    try {
+        const { name, description } = await req.json();
+
+        const category = await Category.create({ name });
+
+        return NextResponse.json({ category }, { status: 201 });
+
+    } catch(error: any) {
+        //console.log(error);
+        return NextResponse.json({ err: error.message }, { status: 500 });
+    }
+}
