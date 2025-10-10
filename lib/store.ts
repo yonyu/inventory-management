7edf61a8857ad07799de1d731e68dd42c0bfbdcd
Yong Yu <ypyu@hotmail.com>
2025-10-09 21:44:20 -0700

    Implement redux reducer for Category

--- lib/store.ts
+++ lib/store.ts
@@ -4,12 +4,14 @@ import type { Action, ThunkAction } from "@reduxjs/toolkit";
 import { combineSlices, configureStore } from "@reduxjs/toolkit";
 import { counterSlice } from "./features/counter/counterSlice";
 import { quotesApiSlice } from "./features/quotes/quotesApiSlice";
+import { categoriesApiSlice } from "./features/categories/categoriesApiSlice";
 
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
 const rootReducer = combineSlices(
   counterSlice, 
   quotesApiSlice, 
+  categoriesApiSlice,
 );
 
 // Infer the `RootState` type from the root reducer
