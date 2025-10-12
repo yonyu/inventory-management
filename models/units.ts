4e58bd4b2cfcdfcefdf1a9fc66cf14a9cc691a3d
Yong Yu <ypyu@hotmail.com>
2025-10-11 21:42:32 -0700

    Create Unit model

---
+++ models/units.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+
+const UnitsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    status: {
+        type: Boolean,
+        default: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    }
+}, {timestamps: true});
+
+export default mongoose.models.Units || mongoose.model('Units', UnitsSchema);
