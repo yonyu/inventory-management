b25da11f16cc7246d424d18484e38a9924df402e
Yong Yu <ypyu@hotmail.com>
2025-10-16 16:16:13 -0700

    Create ProductList page

---
+++ app/dashboard/user/products/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Container } from "@mui/material";
+//import Products from "@/components/products/ProductList";
+
+export default function Pos() {
+
+    return (
+        <>
+            <Container maxWidth="xl" sx={{width: "100%", height: "100%"}}>
+                {/* <Products /> */}
+                Products
+            </Container>
+        </>
+    )
+}
