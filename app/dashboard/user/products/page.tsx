b15b34176464fae189671158b07fcd83419a8987
Yong Yu <ypyu@hotmail.com>
2025-10-17 09:27:00 -0700

    Create ProductList UI - Initial code

--- app/dashboard/user/products/page.tsx
+++ app/dashboard/user/products/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 
 import { Container } from "@mui/material";
-//import Products from "@/components/products/ProductList";
+import Products from "@/components/products/ProductList";
 
 export default function Pos() {
 
     return (
         <>
             <Container maxWidth="xl" sx={{width: "100%", height: "100%"}}>
-                {/* <Products /> */}
-                Products
+                <Products />
             </Container>
         </>
     )
