b67cf680b2ce27cefcae736c1851a9a0babd4b5a
Yong Yu <ypyu@hotmail.com>
2025-10-24 18:01:31 -0700

    Fix the error: params should be awaited before using its properties

--- app/api/user/unit/[id]/route.ts
+++ app/api/user/unit/[id]/route.ts
@@ -5,12 +5,13 @@ import dbConnect from "@/utils/dbConnect";
 import Unit from "@/models/unit";
 
 
-export async function PUT( req: Request, { params }: { params: { id: string } }) {
+export async function PUT( req: Request, { params }: { params: Promise<{ id: string }> }) {
     await dbConnect();
 
     try {
+        const { id } = await params;
         const { ...updateBody } = await req.json();
-        const updatingUnit = await Unit.findByIdAndUpdate(params.id, updateBody, { new: true });
+        const updatingUnit = await Unit.findByIdAndUpdate(id, updateBody, { new: true });
 
         return NextResponse.json(updatingUnit); //({ updatingUnit }, { status: 200 });
     } catch (error: any) {
@@ -19,11 +20,12 @@ export async function PUT( req: Request, { params }: { params: { id: string } })
     } 
 }
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(req: Request, { params }: { params: Promise<{ id: string }> }) {
     await dbConnect();
 
     try {
-        const deletingUnit = await Unit.findByIdAndDelete(params.id);
+        const { id } = await params;
+        const deletingUnit = await Unit.findByIdAndDelete(id);
 
         return NextResponse.json({ deletingUnit }, { status: 200 });
     } catch (error: any) {
