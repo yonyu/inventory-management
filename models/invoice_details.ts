2161b4cd216e02ebf2c9dcdb022a986f0ea8a044
Yong Yu <ypyu@hotmail.com>
2025-10-27 13:05:48 -0700

    Creaate Invoice Details model - Finalize the fields

--- models/invoice_details.ts
+++ models/invoice_details.ts
@@ -1,13 +1,23 @@
 import mongoose from "mongoose";
 
+import Invoice from "./invoice";
 import Category from "./category";
 import Product from "./product";
 
 const InvoiceDetailsSchema = new mongoose.Schema({
+    date: {
+        type: Date,
+        required: true,
+    },
     invoice: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Invoice",
-        required: true,
+        //required: true,
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        //required: true,
     },
     product: {
         type: mongoose.Schema.Types.ObjectId,
@@ -22,26 +32,30 @@ const InvoiceDetailsSchema = new mongoose.Schema({
         type: Number,
         required: true,
     },
-    total_cost: {
-        type: Number,
-        required: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now,
-    },
-    deletedAt: {
-        type: Date,
-        default: null,
-    },
-    deleted: {
+    status: {
         type: Boolean,
-        default: false,
+        default: true,
     },
+    // total_cost: {
+    //     type: Number,
+    //     required: true,
+    // },
+    // createdAt: {
+    //     type: Date,
+    //     default: Date.now,
+    // },
+    // updatedAt: {
+    //     type: Date,
+    //     default: Date.now,
+    // },
+    // deletedAt: {
+    //     type: Date,
+    //     default: null,
+    // },
+    // deleted: {
+    //     type: Boolean,
+    //     default: false,
+    // },
 }, {timestamps: true});
 
 
