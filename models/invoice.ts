899f9c893ee5d0e1c3c88ed63159ef2ced444788
Yong Yu <ypyu@hotmail.com>
2025-10-27 11:53:58 -0700

    Create Invoice model: - A simplified version

--- models/invoice.ts
+++ models/invoice.ts
@@ -10,38 +10,44 @@ const InvoiceSchema = new mongoose.Schema({
         type: Date,
         required: true
     },
-    dueDate: {
-        type: Date,
-        required: true
-    },
-    customer: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Customer',
-        required: true
-    },
-    items: [{
-        description: {
-            type: String,
-            required: true
-        },
-        quantity: {
-            type: Number,
-            required: true
-        },
-        price: {
-            type: Number,
-            required: true
-        }
-    }],
-    totalAmount: {
-        type: Number,
+    description: {
+        type: String,
         required: true
     },
     status: {
-        type: String,
-        enum: ['paid', 'pending', 'overdue'],
-        required: true
-    }
-});
+        type: Boolean,
+        default: false
+    },
+
+
+    // dueDate: {
+    //     type: Date,
+    //     required: true
+    // },
+    // customer: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Customer',
+    //     required: true
+    // },
+    // items: [{
+    //     description: {
+    //         type: String,
+    //         required: true
+    //     },
+    //     quantity: {
+    //         type: Number,
+    //         required: true
+    //     },
+    //     price: {
+    //         type: Number,
+    //         required: true
+    //     }
+    // }],
+    // totalAmount: {
+    //     type: Number,
+    //     required: true
+    // }
+
+}, {timestamps: true});
 
 export default mongoose.models.Invoice || mongoose.model('Invoice', InvoiceSchema);
