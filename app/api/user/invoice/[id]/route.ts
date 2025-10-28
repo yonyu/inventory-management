6bb31c61a0fc5bb744e9a1694cfb5fb7ba606797
Yong Yu <ypyu@hotmail.com>
2025-10-27 19:57:22 -0700

    Implement Delete Invoice API

---
+++ app/api/user/invoice/[id]/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+import dbConnect from "@/utils/dbConnect";
+import Invoice from "@/models/invoice";
+import InvoiceDetails from "@/models/invoice_details";
+import Payment from "@/models/payment";
+import PaymentDetails from "@/models/payment_details";
+
+
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+    await dbConnect();
+
+    try {
+        const { id } = await params;
+        const deletingInvoice = await Invoice.findByIdAndDelete(id);
+        if (!deletingInvoice) {
+            return NextResponse.json({ err: "Invoice not found" }, { status: 404 });
+        }
+
+        const deletingInvoiceDetails = await InvoiceDetails.deleteMany({ invoice: id });
+        const payment = await Payment.deleteMany({ invoice: id });
+        const paymentDetails = await PaymentDetails.deleteMany({ invoice: id });
+
+
+        return NextResponse.json({ deletingInvoice: deletingInvoice }, { status: 200 });
+    } catch (error: any) {
+        return NextResponse.json({ err: error.message }, { status: 500 });
+    }
+}
