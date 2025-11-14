f5222cbe411971e009340fe2c771a741efd56b78
Yong Yu <ypyu@hotmail.com>
2025-11-13 23:57:08 -0800

    Create subscription api

---
+++ app/api/user/subscription/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/utils/authOptions";
+
+import dbConnect from "@/utils/dbConnect";
+
+//import User from "@/models/user";
+import Transaction from "@/models/transaction";
+
+
+export async function GET(request: Request) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+        }
+        await dbConnect();
+        const transaction = await Transaction.find({ user: session?.user?._id });
+        return NextResponse.json(transaction, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ err: error instanceof Error ? error.message : 'Unknown error' }, { status: 500 });
+    }
+}
