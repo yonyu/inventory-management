2f9e376676a24ca8209a210252fd946b7a95a4d8
Yong Yu <ypyu@hotmail.com>
2025-11-16 17:51:50 -0800

    Uses Next.js's built-in font optimization feature to load the Inter font from Google Fonts.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,10 @@ import { StoreProvider } from "./StoreProvider";
 import AuthSessionProvider from "./SessionProvider";
 import { ToastContainer } from 'react-toastify';
 
+import { Inter } from "next/font/google";
 import "@/styles/globals.css";
-import styles from "@/styles/layout.module.css";
+//import styles from "@/styles/layout.module.css";
+const inter = Inter({ subsets: ["latin"] });
 
 interface Props {
   readonly children: ReactNode;
@@ -13,13 +15,14 @@ interface Props {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <StoreProvider>
           <AuthSessionProvider>
             <ToastContainer />
-            <section className={styles.container}>
+            {children}
+            {/* <section className={styles.container}>
               <main className={styles.main}>{children}</main>
-            </section>
+            </section> */}
           </AuthSessionProvider>
         </StoreProvider>
       </body>
