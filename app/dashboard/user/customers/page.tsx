a7ff0d0573bd0db67a3bb23142c5003e80916229
Yong Yu <ypyu@hotmail.com>
2025-11-16 22:09:43 -0800

    Update the component layout

--- app/dashboard/user/customers/page.tsx
+++ app/dashboard/user/customers/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { Container } from "@mui/material";
 import Customers from "@/components/customers/CustomerList";
 
 export default function Pos() {
 
     return (
         <>
-            <Container maxWidth="xl" sx={{width: "100%", height: "100%"}}>
-                <Customers />
-            </Container>
+            <Customers />
         </>
     )
 }
--- components/customers/CustomerList.tsx
+++ components/customers/CustomerList.tsx
@@ -232,7 +232,7 @@ const CustomerTable = () => {
     }
 
     return (
-        <Box sx={{ p: 2, maxWidth: "100%", width: "1024px" }} >
+        <Box sx={{ p: 2 }} >
             <Typography variant="h4" sx={{ mb: 2 }}
                 style={{
                     fontSize: "3rem",
